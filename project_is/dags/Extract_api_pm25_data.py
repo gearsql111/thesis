e80d5f11f8f22ecc9bf0e7d0d0d169f17e959f82
gear <gearsql11@gmail.com>
2024-11-08 23:46:37 +0700

    delete the comment

--- project_is/dags/Extract_api_pm25_data.py
+++ project_is/dags/Extract_api_pm25_data.py
@@ -22,47 +22,42 @@ clean_folder = data_folder + 'cleaned'
 
 
 def fetch_data():
-    # check ว่ามี folder หรือไม่ ถ้าไม่มีก็สร้าง folder ใหม่
     if not os.path.exists(data_path):
         os.makedirs(data_path)
 
-    # check ที่เก็บอยู่ในตัวแปร data_folder มีอยู่หรือไม่.
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
 
-    # if not os.path.exists(clean_folder):
-    #     os.makedirs(clean_folder)
 
-    # ดึง files ทั้งหมดที่อยู่ใน folder data_folder
     exit_file_name = os.listdir(data_folder)
     url = 'https://opendata.onde.go.th/dataset/14-pm-25'
-    links = []  # เอาไว้เก็บค่าต่าง ๆ ที่ถูกดึงมาจาก URL
+    links = []
 
     req = requests.get(url, verify=False)
     req.encoding = 'utf-8'
-    # การแปลง HTML เป็นโครงสร้างข้อมูล
+
     soup = BeautifulSoup(req.text, 'html.parser')
 
     og = soup.find('meta', property='og:url')
     base = urlparse(url)
 
-    # loop ข้อมูลทั้งหมดที่มีอยู่ในหน้าเว็บที่แปลงเป็นโครงสร้างข้อมูล
+
     for link in soup.find_all('a'):
-        current_link = link.get('href')  # ดึงค่าจาก a
-        if str(current_link).endswith('csv'):  # check current_link ลงท้ายด้วย .csv รึป่าว
+        current_link = link.get('href') 
+        if str(current_link).endswith('csv'): 
             links.append(current_link)
 
-    for link in links:  # loop แต่ละตัวที่เก็บใน links และแยก url เพื่อเก็บชื่อตัวสุดท้าย
+    for link in links:
         names = link.split('/')[-1]
-        names = names.strip()  # ลบช่องว่าง (whitespace)
+        names = names.strip()
         name = names.replace('pm_data_hourly-', '')
 
-        # ใช้ check ไฟล์ที่ไม่ต้องการให้ดาวน์โหลดซ้ำ
+
         if (name != 'data_dictionary.csv') & (name not in exit_file_name):
             req = requests.get(link, verify=False)
             url_content = req.content
             file_path = data_folder + name
-            # บันทึกไฟล์ CSV
+
             with open(file_path, 'wb') as csv_file:
                 csv_file.write(req.content)
             print(f"ดาวน์โหลดไฟล์ CSV สำเร็จ: {name}")
@@ -88,7 +83,7 @@ with DAG('Extract_api_pm25_data',
     
     trigger_next_transform_pm25_data = TriggerDagRunOperator(
         task_id='feature_engineering_task',
-        trigger_dag_id='transform_pm25_data',  # ID ของ DAG ถัดไป
+        trigger_dag_id='transform_pm25_data', 
     )
 
 start_task >> fetch_data_task >> trigger_next_transform_pm25_data
--- project_is/dags/load_pm25_feature_store.py
+++ project_is/dags/load_pm25_feature_store.py
@@ -1,8 +1,14 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from feast import FeatureStore
 from datetime import datetime, timedelta
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from google.cloud import storage
 
+
+import os
+
+output_folder = '/opt/airflow/dags/data_test/transformed/'
 
 default_args = {
     'owner': 'airflow',
@@ -10,20 +16,63 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-def load_to_feature_store():
-    with open('/path/to/local/transformed_data.json', 'r') as f:
-        transformed_data = f.read()
-    # Load data to feature store, e.g., BigQuery or GCS
-    print("Loading data to feature store...")  # Replace with actual loading code
 
-with DAG('load_pm25_feature_store', 
-         default_args=default_args, 
-         schedule_interval=None, 
-         start_date=datetime(2023, 1, 1), 
+def load_to_bigquery():
+
+    gcs_bucket = 'gear-bucket-gcs' 
+    dataset_id = 'is-my-project-428015.PM25_dataset'
+    table_id = 'pm25_data_hourly'
+
+    client = storage.Client()
+    bucket = client.bucket(gcs_bucket)
+
+    for filename in os.listdir(output_folder):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(output_folder, filename)
+            blob = bucket.blob(f"data/{filename}")
+            blob.upload_from_filename(file_path)
+            print(f"Uploaded {filename} to GCS")
+
+            job_config = {
+                "sourceUris": [f"gs://{gcs_bucket}/data/{filename}"],
+                "destinationTable": {
+                    "projectId": "is-my-project-428015  ",
+                    "datasetId": "PM25_dataset",
+                    "tableId": "pm25_data_hourly",
+                },
+                "sourceFormat": "CSV",
+                "writeDisposition": "WRITE_APPEND", 
+            }
+
+            load_job = BigQueryInsertJobOperator(
+                task_id=f"load_{filename}_to_bigquery",
+                configuration={"load": job_config},
+                location="US",
+            )
+            load_job.execute(context=None) 
+
+
+def register_feature_in_feast():
+    fs = FeatureStore(repo_path="path/to/your/feature_repo")
+    fs.apply()
+    print("Feature registered successfully in Feast")
+
+
+with DAG('load_pm25_data_to_bigquery',
+         default_args=default_args,
+         schedule_interval=None,
+         start_date=datetime(2024, 11, 3),
          tags=['swu'],
          catchup=False) as dag:
-    
-    load_data_task = PythonOperator(
-        task_id='load_to_feature_store',
-        python_callable=load_to_feature_store,
+
+    load_task_to_bigquery = PythonOperator(
+        task_id='load_to_bigquery',
+        python_callable=load_to_bigquery,
     )
+
+    register_feature_task = PythonOperator(
+        task_id='register_feature_in_feast',
+        python_callable=register_feature_in_feast,
+    )
+
+load_task_to_bigquery >> register_feature_task
